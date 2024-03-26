add1c0f011358fc8b754a1928953f6fe3cdff702
Erico de Souza Loewe <erico.loewe@dbccompany.com.br>
2024-03-26 11:09:27 -0300

    react imask

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import Image from "next/image";
 import "./page.scss";
 import { useState } from "react";
-import { chain } from "mathjs";
+import { add, multiply, divide, format } from "mathjs";
+import { useIMask } from "react-imask";
 
 export default function Home() {
   return (
@@ -28,15 +28,15 @@ function Terreno({ mesesAteVender }: any) {
       <div className="row">
         <div className="mb-3 col-md">
           <label className="form-label">ÁREA DO TERRENO</label>
-          <input type="text" className="form-control" placeholder="300,00 m²" value={area} onChange={e => setArea(Number(e.target.value))} />
+          <Input mask="# m²" onChange={setArea} isNumber={true} />
         </div>
         <div className="mb-3 col-md">
           <label className="form-label">VALOR DE AQUISIÇÃO </label>
-          <input type="text" className="form-control" placeholder="R$ 100.000,00" value={valor} onChange={e => setValor(Number(e.target.value))} />
+          <Input mask="R$ #" onChange={setValor} isNumber={true} />
         </div>
         <div className="mb-3 col-md-2">
           <label className="form-label">PREÇO DO M²</label>
-          <h6>R$ {chain(valor).divide(area).format(2).done()} / m²</h6>
+          <h6>R$ {divide(valor, area)} / m²</h6>
         </div>
       </div>
       <div className="row">
@@ -55,10 +55,22 @@ function Terreno({ mesesAteVender }: any) {
       </div>
       <div className="row mb-3">
         <label className="form-label">CUSTO TOTAL TERRENO</label>
-        <h6>R$ {chain(valor).add(chain(valor).multiply(chain(itbi).add(escrituraERegistro).done()).add(chain(iptu).multiply(chain(mesesAteVender).divide(12).done()).done()).done()).format(2).done()}</h6>
+        <h6>R$ {format(add(add(valor, multiply(valor, add(itbi, escrituraERegistro))), multiply(iptu, divide(mesesAteVender, 12))), 2)}</h6>
         {/* <h6>R$ {(valor + (valor * (itbi + escrituraERegistro)) + (iptu * (mesesAteVender / 12))) || '105.600,00'}</h6> */}
       </div>
     </section>
   )
 }
 
+function Input({ mask, onChange, isNumber }: any) {
+  const { ref } = useIMask({ mask, radix: '.', lazy: false, blocks: { '#': { mask: Number, expose: true, }, } }, { onAccept: onChangeInput });
+
+  function onChangeInput(value: string) {
+    if (isNumber)
+      onChange(Number(value.replaceAll(',', '.')));
+    else
+      onChange(value);
+  }
+
+  return <input ref={ref as any} className="form-control" />;
+}
