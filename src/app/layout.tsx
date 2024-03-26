4b4fd156e6d13213d4e3fb10d4fee47929e12c36
Erico de Souza Loewe <erico.loewe@dbccompany.com.br>
2024-03-26 16:40:52 -0300

    terreno

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,18 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.scss";
+import BigNumber from "bignumber.js";
 
 const inter = Inter({ subsets: ["latin"] });
+
+BigNumber.config({
+  FORMAT: {
+    // decimal separator
+    decimalSeparator: ',',
+    // grouping separator of the integer part
+    groupSeparator: '.',
+  }
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import "./page.scss";
-import { useState } from "react";
-import { add, multiply, divide, format } from "mathjs";
+import { useEffect, useState } from "react";
+// import { add, multiply, divide, format } from "mathjs";
+
+import BigNumber from 'bignumber.js';
 import extenso from "extenso";
 
 export default function Home() {
@@ -14,11 +16,25 @@ export default function Home() {
   );
 }
 function Terreno({ mesesAteVender }: any) {
-  const [area, setArea] = useState<number>(0);
-  const [valor, setValor] = useState<number>(0);
-  const [itbi, setItbi] = useState<number>(0);
-  const [escrituraERegistro, setEscrituraERegistro] = useState<number>(0);
-  const [iptu, setIptu] = useState<number>(0);
+  const [area, setArea] = useState<BigNumber>();
+  const [valor, setValor] = useState<BigNumber>();
+  const [itbi, setItbi] = useState<BigNumber>();
+  const [escrituraERegistro, setEscrituraERegistro] = useState<BigNumber>();
+  const [iptu, setIptu] = useState<BigNumber>();
+  const [valorTotal, setValorTotal] = useState<BigNumber>();
+
+  useEffect(() => {
+    if (valor && itbi && escrituraERegistro && iptu && mesesAteVender) {
+      const mesesAteVenderBig = BigNumber(mesesAteVender);
+      const mesesDoAno = BigNumber(12);
+
+      /* <h6>R$ {(valor + (valor * (itbi + escrituraERegistro)) + (iptu * (mesesAteVender / 12))) || '105.600,00'}</h6> */
+
+      setValorTotal(valor.plus(valor.times(itbi.plus(escrituraERegistro))).plus(iptu.times(mesesAteVenderBig.div(mesesDoAno))));
+    }
+
+  }, [valor, itbi, escrituraERegistro, iptu, mesesAteVender]);
+
 
   return (
     <section className="card valor-terreno">
@@ -32,22 +48,22 @@ function Terreno({ mesesAteVender }: any) {
         </div>
         <div className="mb-3 col-md">
           <label className="form-label">VALOR DE AQUISIÇÃO </label>
-          <Input onChange={setValor} type="number" min="1" step="any" groupSymbolLeft="R$" />
-          <div className="form-text">{!!valor && extenso(valor, { mode: 'currency' })}</div>
+          <Input onChange={setValor} type="number" min="1" groupSymbolLeft="R$" />
+          <div className="form-text">{!!valor && extenso(valor.integerValue().toNumber(), { mode: 'currency' })}</div>
         </div>
         <div className="mb-3 col-md-2">
           <label className="form-label">PREÇO DO M²</label>
-          <h6>R$ {area && valor && format(divide(valor, area), 2)} / m²</h6>
+          <h6>R$ {area && valor && valor.div(area).toFormat(2)} / m²</h6>
         </div>
       </div>
       <div className="row">
         <div className="mb-3 col-md">
           <label className="form-label">ITBI</label>
-          <Input onChange={setItbi} type="number" min="1" step="any" groupSymbolLeft="%" />
+          <Input onChange={setItbi} type="number" min="1" step="any" groupSymbolRight="%" isPercent />
         </div>
         <div className="mb-3 col-md">
           <label className="form-label">ESCRITURA E REGISTRO</label>
-          <Input onChange={setEscrituraERegistro} type="number" min="1" step="any" groupSymbolLeft="%" />
+          <Input onChange={setEscrituraERegistro} type="number" min="1" step="any" groupSymbolRight="%" isPercent />
         </div>
         <div className="mb-3 col-md">
           <label className="form-label">IPTU</label>
@@ -56,21 +72,25 @@ function Terreno({ mesesAteVender }: any) {
       </div>
       <div className="row mb-3">
         <label className="form-label">CUSTO TOTAL TERRENO</label>
-        <h6>R$ {format(add(add(valor, multiply(valor, add(itbi, escrituraERegistro))), multiply(iptu, divide(mesesAteVender, 12))), 2)}</h6>
+        {/* <h6>R$ {format(add(add(valor, multiply(valor, add(itbi, escrituraERegistro))), multiply(iptu, divide(mesesAteVender, 12))), 2)}</h6> */}
         {/* <h6>R$ {(valor + (valor * (itbi + escrituraERegistro)) + (iptu * (mesesAteVender / 12))) || '105.600,00'}</h6> */}
+        <h6>R$ {valorTotal?.toFormat(2)}</h6>
+        <div className="form-text">{!!valorTotal && extenso(valorTotal.integerValue().toNumber(), { mode: 'currency' })}</div>
       </div>
     </section>
   )
 }
 
 function Input(props: any) {
-  const { onChange, isNumber, groupSymbolLeft, groupSymbolRight, ...otherProps } = props;
+  const { onChange, isNumber, groupSymbolLeft, groupSymbolRight, isPercent, ...otherProps } = props;
 
   function onChangeInput(event: any) {
     const { value } = event.target
 
-    if (props.type === 'number')
-      onChange(Number(value));
+    if (isPercent)
+      onChange(BigNumber(value).div(100));
+    else if (props.type === 'number')
+      onChange(BigNumber(value));
     else
       onChange(value);
   }
