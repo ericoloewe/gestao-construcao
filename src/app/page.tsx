100e0d8e8a6bda068b3f576510ceadf7c35951cf
Erico de Souza Loewe <erico.loewe@dbccompany.com.br>
2024-04-24 16:32:42 -0300

    perguntas e respostas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,10 +59,44 @@ function Home() {
               )
           )}
       </section>
+      <section className="qea my-4">
+        <h3 className="my-4">Perguntas e respostas</h3>
+        <ul className="list-group">
+          <li className="list-group-item">
+            <h5>Como faço para criar um orçamento detalhado?</h5>
+            Para criar um orçamento detalhado, acesse a seção de orçamentos no sistema. Insira os itens necessários, como materiais, mão de obra e outros recursos. O sistema calculará automaticamente os custos com base nas quantidades e preços definidos.
+          </li>
+          <li className="list-group-item">
+            <h5>Posso controlar os gastos por centro de custo?</h5>
+            Sim! O sistema permite que você associe cada despesa a um centro de custo específico. Dessa forma, você pode acompanhar os gastos de acordo com as diferentes áreas do projeto.
+          </li>
+          <li className="list-group-item">
+            <h5>Como faço para solicitar cotações de fornecedores?</h5>
+            Na seção de compras, você pode criar solicitações de cotação para os materiais necessários. O sistema enviará essas solicitações aos fornecedores cadastrados, facilitando o processo de comparação e escolha.
+          </li>
+          <li className="list-group-item">
+            <h5>Como acompanho o progresso da obra?</h5>
+            O sistema possui um módulo de acompanhamento de obra. Nele, você pode inserir informações sobre o avanço físico, marcos importantes e atualizações do cronograma. Isso ajuda a manter todos os envolvidos informados.
+          </li>
+          <li className="list-group-item">
+            <h5>Existe um portal do cliente para compartilhar informações?</h5>
+            Sim! O portal do cliente permite que os clientes acessem informações relevantes sobre a obra, como fotos, relatórios de progresso e documentos. É uma maneira eficiente de manter a transparência e a comunicação.
+          </li>
+          <li className="list-group-item">
+            <h5>Como registro informações sobre os funcionários?</h5>
+            No módulo de gestão de funcionários, você pode adicionar detalhes sobre cada membro da equipe, incluindo dados pessoais, documentos e alocação em projetos específicos.
+          </li>
+          <li className="list-group-item">
+            <h5>O sistema se integra com outras ferramentas?</h5>
+            Sim! Ele pode ser integrado com outros módulos, como recursos humanos, suprimentos e financeiro. Isso garante uma visão completa e centralizada de todas as operações.
+          </li>
+        </ul>
+      </section>
     </main>
   );
-
 }
+
+
 
 export default function Page() {
 
